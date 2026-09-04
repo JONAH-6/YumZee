9a5df527bc327b313352015a3872bbfa908b0296
JONAH-6 <maziemeka6@gmail.com>
2026-09-04 15:26:08 +0100

    Add draggable floating cart button

--- web/src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ web/src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -38,7 +38,7 @@ const ProductDetailPage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-[#FFF9E5] pb-20">
+    <div className="min-h-screen bg-[#FFF9E5] pb-10">
       <Metadata
         title={`Yumzee — ${product.name}`}
         description={`Product detail for ${product.name}`}
